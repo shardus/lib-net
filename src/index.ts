ef17f62fd48e03469612292c7526a93955c1088b
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-08-23 22:58:06 -0500

    various comments related to future upgrades

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,18 @@ export interface AugmentedData {
   UUID: string
   PORT: Port
   ADDRESS?: Address
+
+
+
+  //here are some timestamps we can add to learn more about the time it takes for a TX
+  //to move through the system
+  //sendTime : timestamp of when we receive the TX
+
+  //not certain we have a great way to connect these yet 
+  //receivedTime: timestamp of then we got the message
+  //responseTime:  if this is a response we can log when did we send the response
+
+  //should have on option to log aug data for every incoming and outgoing message
 }
 
 export interface RemoteSender {
@@ -68,6 +80,13 @@ export type SnOpts = {
   customStringifier?: (val: any) => string
 }
 
+//TODO_HEADERS we the ability to change if sending with headers is enabled.
+//new nodes will rotate in with the ability to do so, but we will need to let shardeum
+//migration control when this feature becomes active. 
+//basically needs some an exposed method to allow setting this flag
+//receiving either type of message is non breaking if we do it right.
+
+
 // We have to generate a closure so,
 // 1) We can test simulated from two isolated environments, and
 // 2) Users can use two distinct copies if they want to, for whatever
@@ -77,6 +96,7 @@ export type SnOpts = {
 // the `send` function can augment its sent data with the port the `listen`
 // function will be listening on. This is necessary to simulate "responding"
 // to a "request".
+
 export const Sn = (opts: SnOpts) => {
   validate(opts)
 
@@ -107,6 +127,17 @@ export const Sn = (opts: SnOpts) => {
 
   const retainTimedOutEntriesForMillis = 1000 * 60
 
+  /**
+   * This sends our data the that is wrapped in an augData structure
+   * a new function will be added similar to this one to send data with headers
+   * @param port 
+   * @param address 
+   * @param augData 
+   * @param timeout 
+   * @param onResponse 
+   * @param onTimeout 
+   * @returns 
+   */
   const _sendAug = async (
     port: number,
     address: string,
@@ -148,10 +179,18 @@ export const Sn = (opts: SnOpts) => {
           )
           /* prettier-ignore */ console.log(`_sendAug: request id ${augData.UUID}: timed out after ${Date.now() - mapping.timestamp}ms`)
           /* prettier-ignore */ console.log(`_sendAug: request id ${augData.UUID}: detailed aug data: ${JSON.stringify(augData)}`)
+
+          //should we clear the request socket here?
+          //should be be logging better at this level
+          //maybe a counter service could be passed to this library?
+
           delete responseUUIDMapping[augData.UUID]
           onTimeout()
         }, timeout)
 
+        // this is where we bind the response callback to the UUID
+        // later extractUUIDHandleData will call this callback if it
+        // finds a UUID match.
         responseUUIDMapping[augData.UUID] = {
           callback: (data: unknown) => {
             clearTimeout(timer)
@@ -163,6 +202,18 @@ export const Sn = (opts: SnOpts) => {
     })
   }
 
+  //TODO_HEADERS create a new _sendWithHeaders function that can send data with a header object.
+  //We will use versioned binary headers but that will be handled in rust.
+  //The data passed in for the message should be a json object.  initially this will still use
+  //JSON, but some messages will get converted to binary most likely on the rust side of things for
+  //performance reasons, but we will need to test this. 
+
+
+  // TODO_HEADERS I think we may need to send asks in the future with a node ID as well if we want to check 
+  // for an already existing socket connection
+  // need to sort out the security though or else a node may be able to fake owning an ID unless we actually verify one
+  // signature from it. maybe the first header needs a signature? or maybe we need some way to ask shardus core
+  // if an incoming message from a certain public key is valid. This is a bit complex 
   const send = async (
     port: number,
     address: string,
@@ -173,12 +224,17 @@ export const Sn = (opts: SnOpts) => {
   ) => {
     const UUID = uuid()
 
+    //TODO_HEADERS sending a port and address seems wrong here!! the response should come back over the existing socket
+    //seems like this was written as if we are udp
+
     // Under the hood, sn needs to pass around some extra data for its own internal usage.
     const augData: AugmentedData = {
       data, // the user's data
       UUID, // the ID we'll use to determine whether requests were "responded to"
       PORT, // the listening port,    so the library knows to whom to send "responses" to
       ADDRESS, // the listening address, although the library will use the address it got from the network
+
+                                                                        
     }
 
     return _sendAug(port, address, augData, timeout, onResponse, onTimeout)
@@ -199,6 +255,9 @@ export const Sn = (opts: SnOpts) => {
       // This is the return send function. A user will call this if they want
       // to "reply" or "respond" to an incoming message.
       const respond: ResponseCallback = (data: unknown) => {
+
+        //we can do some timestamp work here for better logging.
+
         const sendData = { data, UUID, PORT }
         //@ts-ignore TODO: FIX THISSSSSS (Remove the ignore flag and make typescript not complain about address being possibly undefined)
         // @TODO: This error should be properly propagated and logged.
@@ -233,9 +292,17 @@ export const Sn = (opts: SnOpts) => {
       return handle(data, remote, respond)
     }
 
+    // OLD comment from initial implementation:
     // TODO these should be spun up in parallel, but that convolutes code
     // and doesn't save hardly any startup time, so skipping for now.
     // const server = await _net.listen(PORT, ADDRESS, extractUUIDHandleData)
+
+    //args to the callback function in listen
+    //let args: [Handle<JsValue>; 3] = [message.upcast(), remote_ip.upcast(), remote_port.upcast()];
+
+    // TODO_HEADERS: extractUUIDHandleData will will have to be swapped with something a step up 
+    // that can check the first byte and determine if we use the old json protocol or a new protocol
+    // with headers
     const server = await _net.listen((data, remoteIp, remotePort) => {
       extractUUIDHandleData(data, {
         address: remoteIp,
