e2f1d16946e51d7077f924c8c868fb918427cd99
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-08-24 03:02:53 -0500

    a few minor log adjustments , added test2

--- src/index.ts
+++ src/index.ts
@@ -58,24 +58,31 @@ function logMessageInfo(augData: AugmentedData, stringifiedData:string, sending:
   let logMsg = `netmsglog: ${sendingStr} ${augData.msgDir}: ${logData} UUID: ${augData.UUID} PORT: ${augData.PORT} ADDRESS: ${augData.ADDRESS}`
   
   if(augData.msgDir === 'tell'){
-    //log timestamps for sendTime
-    logMsg += ` sendTime:${augData.sendTime}`
-    if (sending === false) {
-      logMsg += ` recvTime:${receivedTime} recvDelta:${receivedTime - augData.sendTime}`
+    if(augData.sendTime != null){
+      //log timestamps for sendTime
+      logMsg += ` sendTime:${augData.sendTime}`
+      if (sending === false) {
+        logMsg += ` recvTime:${receivedTime} recvDelta:${receivedTime - augData.sendTime}`
+      }
     }
   } else if( augData.msgDir === 'ask') {
-    logMsg += ` sendTime:${augData.sendTime}`
-    if (sending === false) {
-      logMsg += ` recvTime:${receivedTime} recvDelta:${receivedTime - augData.sendTime}`
+    if(augData.sendTime != null){
+      logMsg += ` sendTime:${augData.sendTime}`
+      if (sending === false) {
+        logMsg += ` recvTime:${receivedTime} recvDelta:${receivedTime - augData.sendTime}`
+      }
     }
   } else if(augData.msgDir === 'resp') {
-    //reply delta is interesting as it is the time needed for the software to get the reply ready
-    logMsg += ` sendTime:${augData.sendTime} replyTime:${augData.replyTime} replyDelta:${augData.replyTime - augData.receivedTime} `
-    if (sending === false) {
-      // note the ask is how long it took for the original ask to get a reply, not the same as recvDelta, same code but but run at a different time/state
-      logMsg += ` recvTime:${receivedTime} askDelta:${receivedTime - augData.sendTime}`
-      logMsg += ` replyRecvTime:${augData.replyReceivedTime} replyRecvDelta:${receivedTime - augData.replyReceivedTime}`
+    if(augData.sendTime != null){
+      //reply delta is interesting as it is the time needed for the software to get the reply ready
+      logMsg += ` sendTime:${augData.sendTime} replyTime:${augData.replyTime} replyDelta:${augData.replyTime - augData.receivedTime} `
+      if (sending === false) {
+        // note the ask is how long it took for the original ask to get a reply, not the same as recvDelta, same code but but run at a different time/state
+        logMsg += ` recvTime:${receivedTime} askDelta:${receivedTime - augData.sendTime}`
+        logMsg += ` replyRecvTime:${augData.replyReceivedTime} replyRecvDelta:${receivedTime - augData.replyReceivedTime}`
+      }      
     }
+
   }
   
   console.log(logMsg)
