f8cf1eb3bae1322012f008f57a22ac6714f44a48
sabdulazeem01 <sabdulazeem01@gmail.com>
2023-09-04 14:48:39 +0530

    Add headerV1 interface in typescript

--- src/headers.ts
+++ src/headers.ts
@@ -1,3 +1,10 @@
 export interface Headers {
   [key: string]: string;
 }
+export interface HeaderV1 {
+  senderAddress: Uint8Array;  // Using Uint8Array for [u8; 32]
+  uuid: string;  // Assuming we will convert Uuid to a string representation
+  messageType: number;
+  messageLength: number;
+  authorizationData: Uint8Array;  // Using Uint8Array for Vec<u8>
+}
