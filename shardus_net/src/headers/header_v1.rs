ff92edc55e555b5bef98de436e039ed1407cfe20
Arham Jain <arham.nec@gmail.com>
2023-09-05 23:56:32 +0530

    feat(str): added sender address string to hv1 and removed dep on serde for json serialise

--- shardus_net/src/headers/header_v1.rs
+++ shardus_net/src/headers/header_v1.rs
@@ -3,14 +3,15 @@ use uuid::Uuid;
 extern crate serde;
 extern crate serde_json;
 
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub struct HeaderV1 {
     pub uuid: Uuid,
     pub message_type: u32,
     #[serde(default)]
     pub message_length: u32,
+    pub sender_address: String,
 }
 
 impl HeaderV1 {
@@ -26,6 +27,12 @@ impl HeaderV1 {
 
         // Serialize message_length (4 bytes)
         buffer.write_all(&self.message_length.to_le_bytes()).unwrap();
+
+        // Serialize sender_address as bytes and then its length (4 bytes)
+        let sender_address_bytes = self.sender_address.as_bytes();
+        let sender_address_len = sender_address_bytes.len() as u32;
+        buffer.write_all(&sender_address_len.to_le_bytes()).unwrap();
+        buffer.write_all(sender_address_bytes).unwrap();
 
         buffer
     }
@@ -47,7 +54,21 @@ impl HeaderV1 {
         cursor.read_exact(&mut message_length_bytes).ok()?;
         let message_length = u32::from_le_bytes(message_length_bytes);
 
-        Some(Self { uuid, message_type, message_length })
+        // Deserialize sender_address
+        let mut sender_address_len_bytes = [0u8; 4];
+        cursor.read_exact(&mut sender_address_len_bytes).ok()?;
+        let sender_address_len = u32::from_le_bytes(sender_address_len_bytes);
+
+        let mut sender_address_bytes = vec![0u8; sender_address_len as usize];
+        cursor.read_exact(&mut sender_address_bytes).ok()?;
+        let sender_address = String::from_utf8(sender_address_bytes).ok()?;
+
+        Some(Self {
+            uuid,
+            message_type,
+            message_length,
+            sender_address,
+        })
     }
 
     pub fn from_json_string(json_str: &str) -> Option<Self> {
@@ -55,21 +76,25 @@ impl HeaderV1 {
     }
 
     pub fn to_json_string(&self) -> Option<String> {
-        serde_json::to_string(self).ok()
+        Some(format!(
+            r#"{{"uuid": "{}", "message_type": {}, "message_length": {}, "sender_address": "{}"}}"#,
+            self.uuid, self.message_type, self.message_length, self.sender_address
+        ))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use uuid::Uuid;
+    use std::io::Cursor;
 
     #[test]
-    fn test_serialize_deserialize_header_v1() {
+    fn test_serialize_and_deserialize() {
         let header = HeaderV1 {
             uuid: Uuid::new_v4(),
-            message_type: 42,
-            message_length: 100,
+            message_type: 1,
+            message_length: 42,
+            sender_address: "127.0.0.1".to_string(),
         };
 
         let serialized = header.serialize();
@@ -79,5 +104,38 @@ mod tests {
         assert_eq!(header.uuid, deserialized.uuid);
         assert_eq!(header.message_type, deserialized.message_type);
         assert_eq!(header.message_length, deserialized.message_length);
+        assert_eq!(header.sender_address, deserialized.sender_address);
+    }
+
+    #[test]
+    fn test_from_json_string() {
+        let json_str = r#"{
+            "uuid": "550e8400-e29b-41d4-a716-446655440000",
+            "message_type": 1,
+            "message_length": 42,
+            "sender_address": "127.0.0.1"
+        }"#;
+
+        let header = HeaderV1::from_json_string(json_str).unwrap();
+        assert_eq!(header.uuid, Uuid::parse_str("550e8400-e29b-41d4-a716-446655440000").unwrap());
+        assert_eq!(header.message_type, 1);
+        assert_eq!(header.message_length, 42);
+        assert_eq!(header.sender_address, "127.0.0.1");
+    }
+
+    #[test]
+    fn test_to_json_string() {
+        let header = HeaderV1 {
+            uuid: Uuid::parse_str("550e8400-e29b-41d4-a716-446655440000").unwrap(),
+            message_type: 1,
+            message_length: 42,
+            sender_address: "127.0.0.1".to_string(),
+        };
+
+        let json_str = header.to_json_string().unwrap();
+        assert_eq!(
+            json_str,
+            r#"{"uuid": "550e8400-e29b-41d4-a716-446655440000", "message_type": 1, "message_length": 42, "sender_address": "127.0.0.1"}"#
+        );
     }
 }
